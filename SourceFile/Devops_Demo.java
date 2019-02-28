8f7a0f9c2685a477fac202834a74dded22bf56c5
Rashmi A P <rashmiap21@gmail.com>
2019-02-28 14:45:43 +0530

    asdad
    asd

--- SourceFile/Devops_Demo.java
+++ SourceFile/Devops_Demo.java
@@ -1,7 +1,5 @@
 class Devops_Demo {
     public static void main(String[] args) {
         System.out.println("Devops Demo Java File"); 
-		//For BD1912 - Incident 1234
-        System.out.println("Changes for incident 1234"); 
     }
 }
